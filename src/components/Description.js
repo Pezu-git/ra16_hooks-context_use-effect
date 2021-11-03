663be2b0a0d6582c36c63b975d8c5be10aec799b
Pezu-git <artem_nikolaev69@mail.ru>
2021-11-03 17:08:06 +0300

    first commit

---
+++ src/components/Description.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Description = ({desc, text}) => {
+  return (
+    <li className="list-group-item">
+      <span className="text-capitalize">{desc}</span>:
+      <span className="text-capitalize">{` ${text}`}</span>
+    </li>
+  );
+};
+
+Description.propTypes = {
+  desc: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired
+}
+
+export default DescriptionDetail;
